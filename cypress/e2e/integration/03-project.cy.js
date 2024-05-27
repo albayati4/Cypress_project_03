49f9840ce6857cc00d9cb93f8252cf77e03035c2
Abdullah Albayati <dark.king1154@gmail.com>
2024-05-27 15:11:30 -0500

    Completed All test cases

---
+++ cypress/e2e/integration/03-project.cy.js
@@ -0,0 +1,117 @@
+/// <reference types="cypress"/>
+import BookYourTrip from "../pages/BookYourTrip";
+const date = new Date();
+const bookYourTrip = new BookYourTrip();
+
+describe("Cypress Project 03 - submission and date-picking processes", () => {
+  beforeEach(() => {
+    cy.visit("https://techglobal-training.com/frontend/project-3");
+  });
+  it("Test Case 01 - Validate the default Book your trip form", () => {
+    bookYourTrip.getOneWayRadio().should("be.visible").and("be.checked");
+    bookYourTrip.getRoundTripRaido().should("be.visible").and("not.be.checked");
+    bookYourTrip.getFirstThreeDropDowns().then((firstThreeDropDowns) => {
+      cy.wrap(firstThreeDropDowns).each(($el) => {
+        cy.wrap($el).should("be.visible");
+      });
+    });
+    bookYourTrip.getDepartAndReturnDate().then((departAndReturnDate) => {
+      cy.wrap(departAndReturnDate).each(($el) => {
+        cy.wrap($el).should("be.visible");
+      });
+    });
+    bookYourTrip.getNumOfPassangers().should("be.visible").and("have.value", 1);
+    bookYourTrip.getFirstPassanger().should("be.visible").and("have.value", "Adult (16-64)");
+    bookYourTrip.getBookButton().should("be.visible").and("not.be.disabled");
+  });
+
+  it("Test Case 02 - Validate the Book your trip form when Round trip is selected", () => {
+    bookYourTrip.getRoundTripRaido().realClick().should("be.checked");
+    bookYourTrip.getOneWayRadio().should("not.be.checked");
+    bookYourTrip.getFirstThreeDropDowns().then((firstThreeDropDowns) => {
+      cy.wrap(firstThreeDropDowns).each(($el) => {
+        cy.wrap($el).should("be.visible");
+      });
+    });
+    bookYourTrip.getDepartAndReturnDate().then((departAndReturnDate) => {
+      cy.wrap(departAndReturnDate).each(($el) => {
+        cy.wrap($el).should("be.visible");
+      });
+    });
+    bookYourTrip.getNumOfPassangers().should("be.visible").and("have.value", 1);
+    bookYourTrip.getFirstPassanger().should("be.visible").and("have.value", "Adult (16-64)");
+    bookYourTrip.getBookButton().should("be.visible").and("not.be.disabled");
+  });
+
+  it("Test Case 03 - Validate the booking for 1 passenger and one way", () => {
+    bookYourTrip.getOneWayRadio().check();
+    bookYourTrip.getCabinClass().realClick().select("Business");
+    bookYourTrip.getFromDropDown().realClick().select("Illinois");
+    bookYourTrip.getToDropDown().realClick().select("Florida");
+    bookYourTrip.getFromDate().realClick().clear().type(bookYourTrip.getNewDate("06", "04", "2024"));
+    bookYourTrip.getNumOfPassangers().should("be.visible").and("have.value", 1);
+    bookYourTrip.getFirstPassanger().realClick().select("Senior (65+)");
+    bookYourTrip.getBookButton().realClick();
+    bookYourTrip.getDepart().should("have.text", "DEPART");
+    bookYourTrip.getDestination().should("have.text", "IL to FL");
+    bookYourTrip.getFlightDetails().each(($el, index) => {
+      const arr = ["Number of Passengers: 1", "Passenger 1: Senior (65+)", "Cabin class: Business"];
+      cy.wrap($el).should("have.text", arr[index]);
+    });
+  });
+
+  it("Test Case 04 - Validate the booking for 1 passenger and round trip", () => {
+    bookYourTrip.getRoundTripRaido().check();
+    bookYourTrip.getCabinClass().select("First");
+    bookYourTrip.getFromDropDown().select("California");
+    bookYourTrip.getToDropDown().select("Illinois");
+    bookYourTrip.getFromDate().realClick().clear().type(bookYourTrip.getNewDate("06", "04", "2024"));
+    bookYourTrip.getReturnDate().realClick().clear().type(bookYourTrip.getNewDate("07", "04", "2024"));
+    bookYourTrip.getBookButton().realClick();
+    bookYourTrip.getDepart().should("be.visible");
+    bookYourTrip.getDestination().should("have.text", "CA to ILIL to CA");
+    bookYourTrip.getFlightDetails().each(($el, index) => {
+      const arr = ["Number of Passengers: 1", "Passenger 1: Adult (16-64)", "Cabin class: First"];
+      cy.wrap($el).should("have.text", arr[index]);
+    });
+  });
+
+  it("Test Case 05 - Validate the booking for 2 passengers and one way", () => {
+    bookYourTrip.getOneWayRadio().check();
+    bookYourTrip.getCabinClass().select("Premium Economy");
+    bookYourTrip.getFromDropDown().select("New York");
+    bookYourTrip.getToDropDown().select("Texas");
+    bookYourTrip.getFromDate().clear().type(bookYourTrip.getNewDate("06", "28", "2024"));
+    bookYourTrip.getNumOfPassangers().select(1, { force: true });
+    bookYourTrip.getFirstPassanger().select("Adult (16-64)");
+    bookYourTrip.getSecondPassanger().select("Child (2-11)");
+    bookYourTrip.getBookButton().realClick();
+    bookYourTrip.getDepart().should("have.text", "DEPART");
+    bookYourTrip.getDestination().should("have.text", "NY to TX");
+    bookYourTrip.getFlightDetails().each(($el, index) => {
+      const arr = ["Number of Passengers: 2", "Passenger 1: Adult (16-64)", "Passenger 2: Child (2-11)", "Cabin class: Premium Economy"];
+      cy.wrap($el).should("have.text", arr[index]);
+    });
+  });
+});
+
+/*
+Navigate to https://techglobal-training.com/frontend/project-3
+Select the “One way” radio button
+Select “Premium Economy” for the “Cabin Class” dropdown
+Select “New York” for the “From” dropdown
+Select “Texas” for the “To” dropdown
+Select the next day for the ”Depart”
+Select “2” for the “Number of passengers” dropdown
+Select “Adult (16-64)” for the Passenger 1 dropdown
+Select “Child (2-11)” for the Passenger 2 dropdown
+Click on the “BOOK” button
+Validate the booking information displayed below
+DEPART
+NY to TX
+{dynamic date}
+Number of passengers: 2
+Passenger 1: Adult (16-64)
+Passenger 2: Child (2-11)
+Cabin Class: Premium Economy
+*/
--- cypress/e2e/pages/BookYourTrip.js
+++ cypress/e2e/pages/BookYourTrip.js
@@ -1,3 +1,65 @@
-class BookYourTrip {}
+class BookYourTrip {
+  // Locators
+  getOneWayRadio() {
+    return cy.get("[value='One way']");
+  }
+  getRoundTripRaido() {
+    return cy.get('[value="Round trip"]');
+  }
+  getCabinClass() {
+    return cy.get(".select > select").eq(0);
+  }
+  getFromDropDown() {
+    return cy.get(".select > select").eq(1);
+  }
+  getFromDate() {
+    return cy.get('[placeholder="MM/DD/YY"]').eq(0);
+  }
+  getReturnDate() {
+    return cy.get('[placeholder="MM/DD/YY"]').eq(1);
+  }
+  getToDate() {
+    return cy.get('[placeholder="MM/DD/YY"]').eq(1);
+  }
+  getToDropDown() {
+    return cy.get(".select > select").eq(2);
+  }
+  getFirstThreeDropDowns() {
+    return cy.get(".select").then(($el) => {
+      return Cypress.$($el).slice(0, 3).toArray();
+    });
+  }
+  getDepartAndReturnDate() {
+    return cy.get(".label").then(($el) => {
+      return Cypress.$($el).slice(4, 6).toArray();
+    });
+  }
+  getNumOfPassangers() {
+    return cy.get(":nth-child(7) > .select > select");
+  }
+  getFirstPassanger() {
+    return cy.get(":nth-child(8) > .select > select");
+  }
+  getBookButton() {
+    return cy.get(".Button_c_button__TmkRS");
+  }
+  getDepart() {
+    return cy.get(".is-underlined");
+  }
+  getDestination() {
+    return cy.get(".is-italic");
+  }
+  getFlightDetails() {
+    return cy.get(".mt-4 > p");
+  }
+  getSecondPassanger() {
+    return cy.get(":nth-child(9) > .select > select");
+  }
+
+  // Methods
+  getNewDate(month, day, year) {
+    return `${year}-${month}-${day}`;
+  }
+}
 
 export default BookYourTrip;
